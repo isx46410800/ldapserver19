7f535a20d5abd16e492f486984a20fafa765be8d
Miguel Amorós <miguel14amoros@gmail.com>
2019-10-28 01:19:00 +0100

    practica schema

--- ldapserver19:practica/install.sh
+++ ldapserver19:practica/install.sh
@@ -3,7 +3,9 @@
 
 rm -rf /etc/openldap/slapd.d/*
 rm -rf /var/lib/ldap/*
-cp /opt/docker/DB_CONFIG /var/lib/ldap/. 
+cp /opt/docker/DB_CONFIG /var/lib/ldap/.
+cp -rf /opt/docker/config.php /etc/phpldapadmin/.
+cp -rf /opt/docker/phpldapadmin.conf /etc/httpd/config.d/.
 slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d/
 slapadd -F /etc/openldap/slapd.d/ -l /opt/docker/edt.org.ldif
 chown -R ldap.ldap /etc/openldap/slapd.d
